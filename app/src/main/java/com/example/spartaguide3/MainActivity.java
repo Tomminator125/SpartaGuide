0b407d44f5512f5693a4d788188839704d840465
Daniel Nguyen <ravine4675@gmail.com>
2022-04-09 02:45:53 -0400

    second activity

--- app/src/main/java/com/example/spartaguide3/MainActivity.java
+++ app/src/main/java/com/example/spartaguide3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.spartaguide3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
