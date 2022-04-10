e9c3d8063a9d177d238204d20a1e262bfadd11ab
Tom Weir <tom.weir@cyprx.com>
2022-04-10 09:49:32 -0400

    Added code to make lat and long work

--- app/src/main/java/com/example/spartaguide3/FusedLocationProvider.java
+++ app/src/main/java/com/example/spartaguide3/FusedLocationProvider.java
@@ -1,11 +1,13 @@
 package com.example.spartaguide3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -13,6 +15,7 @@ import com.google.android.gms.location.LocationServices;
 public class FusedLocationProvider extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
+//    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,38 @@ public class FusedLocationProvider extends AppCompatActivity {
         setContentView(R.layout.activity_fused_location_provider);
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private int requestCode = 1000;
+    private double userLat = 0.00, userLong = 0.00;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000:
+                if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+                        if (location != null) {
+                            userLat = location.getLatitude();
+                            userLong = location.getLongitude();
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "We need permission please", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
     }
 }
